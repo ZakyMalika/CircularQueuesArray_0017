70ec18484cce27e539f33999bfd81f3340b8b73f
ZakyMalika <zakymalika0987@gmail.com>
2024-05-30 19:52:37 +0700

    membuat fungsi remove untuk mengecek antrian yang kosong

--- circuralqueuesarray/circuralqueuesarray.cpp
+++ circuralqueuesarray/circuralqueuesarray.cpp
@@ -41,6 +41,17 @@ public:
             else
                 REAR = REAR + 1;
         }
+        queue_array[REAR] = num; //menambahkan elemen ke antrian
+    }
+    void remove()
+    {
+        //cek apakah antrian kosong?
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
 
     }
 };
